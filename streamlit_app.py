83cae9ab7e1e95ac7e55a93b6e9878989fb5dc14
Shubham Chouksey <110541556+Shubham-Chouksey@users.noreply.github.com>
2022-08-03 18:30:31 +0530

    Update streamlit_app.py
    Added the menu for break fast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Dosa, Idli and sambhar')
+streamlit.text('Poha, Misla Pav' & Kaju Shake)
+streamlit.text('Eggs & Boliled chicken')
