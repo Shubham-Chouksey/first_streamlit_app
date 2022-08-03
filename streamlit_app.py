96d57dec68277158e9b9a09a5ad30fc9d47fc995
Shubham Chouksey <110541556+Shubham-Chouksey@users.noreply.github.com>
2022-08-03 23:06:11 +0530

    Update streamlit_app.py
    Added Emoji and one Item in menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Dosa, Idli and sambhar')
-streamlit.text('Poha, Misla Pav & Kaju Shake')
-streamlit.text('Eggs & Boliled chicken')
+streamlit.header('🥣 Breakfast menu')
+streamlit.text('🥗 Dosa, Idli and sambhar')
+streamlit.text('🥑 Poha, Misla Pav & Kaju Shake')
+streamlit.text('🐔 Eggs & Boliled chicken')
+streamlit.text('🍞  Avocado Toast')
