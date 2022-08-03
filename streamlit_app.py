55a3461dca86f2ac4333a3afac6bb705c903a909
Shubham Chouksey <110541556+Shubham-Chouksey@users.noreply.github.com>
2022-08-03 23:12:37 +0530

    Update streamlit_app.py
    Insert a new library in the code i.e is pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -9,3 +10,6 @@ streamlit.text('🐔 Eggs & Boliled chicken')
 streamlit.text('🍞  Avocado Toast')
 
 streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list =pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.datafram(my_fruit_list)
