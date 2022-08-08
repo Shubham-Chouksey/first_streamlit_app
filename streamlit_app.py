cd24d867b94d704372890b6d1d9bf275306e2e03
Shubham Chouksey <110541556+Shubham-Chouksey@users.noreply.github.com>
2022-08-08 21:43:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
